c24c0c88535fe30109909d0b8e220ae840080486
ZENABA ABDRAMANE ISSA <zenabaabdramaneissa@gmail.com>
2025-11-16 13:44:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8000440813-18b0a",
+  "appId": "1:227410167668:web:a66265fb01052a59e60e21",
+  "apiKey": "AIzaSyBcwliRGkma16bnpo2sV51FwFgbEbw26bc",
+  "authDomain": "studio-8000440813-18b0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "227410167668"
+};
