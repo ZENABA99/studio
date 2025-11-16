75f30023da70118f9351c8d766ad2134f7002668
ZENABA ABDRAMANE ISSA <zenabaabdramaneissa@gmail.com>
2025-11-16 16:01:59 +0000

    premier commite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ function FeaturedProductsSection() {
         <div className="text-center mt-12">
           <Button asChild size="lg" variant="outline">
             <Link href="/products">
-              Voir tous les produits <ArrowRight className="ml-2 h-5 w-5" />
+              Voire tous les produits <ArrowRight className="ml-2 h-5 w-5" />
             </Link>
           </Button>
         </div>
